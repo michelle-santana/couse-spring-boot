5d51ea73c2b00b9dadf6fc0394c712a8721bd351
michelle-santana <michelle.raiadrogasil@gmail.com>
2021-01-28 11:18:16 -0300

    Order, Instant, ISO 8601 - parte 1

--- src/main/java/com/cousespring/boot/entities/User.java
+++ src/main/java/com/cousespring/boot/entities/User.java
@@ -1,15 +1,19 @@
 package com.cousespring.boot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
-
 	private static final long serialVersionUID = 1L;
 	
 	@Id
@@ -19,6 +23,9 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	   
 	
 	public User() {
@@ -73,6 +80,10 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public List<Order> getOrders() {
+		return orders;
+	}
 
 	@Override
 	public int hashCode() {
@@ -99,10 +110,6 @@ public class User implements Serializable {
 		return true;
 	}
 
-
-	
-	
-	
 	
 
 }
