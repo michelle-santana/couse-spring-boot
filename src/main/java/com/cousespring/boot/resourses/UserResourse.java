49ce99d686dcb177c28bb400eff4e74f2875e210
michelle-santana <michelle.raiadrogasil@gmail.com>
2021-01-28 10:06:49 -0300

    Service layer, component registration

--- src/main/java/com/cousespring/boot/resourses/UserResourse.java
+++ src/main/java/com/cousespring/boot/resourses/UserResourse.java
@@ -1,21 +1,35 @@
 package com.cousespring.boot.resourses;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cousespring.boot.entities.User;
+import com.cousespring.boot.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResourse {
 	
+	@Autowired //injecao dependencia
+	private UserService service; //chamando serviço. dependencia service
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Michelle", "michelle@gmail.com", "999999999", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();				
+		return ResponseEntity.ok().body(list);
 		
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
 }
---
+++ src/main/java/com/cousespring/boot/services/UserService.java
@@ -0,0 +1,29 @@
+package com.cousespring.boot.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cousespring.boot.entities.User;
+import com.cousespring.boot.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	
+	@Autowired
+	private UserRepository repository; //dependencia da classe userRepository
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+		
+	}
+
+}
