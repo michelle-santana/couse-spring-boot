76163f0f36822aff1c8ef7e507fc61e625fcec05
michelle-santana <michelle.raiadrogasil@gmail.com>
2021-01-29 09:10:17 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/cousespring/boot/entities/Product.java
+++ src/main/java/com/cousespring/boot/entities/Product.java
@@ -11,7 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 
 @Entity
@@ -33,6 +36,9 @@ public class Product implements Serializable{
 	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	
 	private Set<Category> categories = new HashSet<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrdemItem> items = new HashSet<>();
 	
 	public Product() {
 		
@@ -89,6 +95,15 @@ public class Product implements Serializable{
 	
 	public Set<Category> getCategories() {
 		return categories;
+	}
+	
+	@JsonIgnore
+	public Set<Order> getOrders(){
+		Set<Order> set = new HashSet<>();
+		for(OrdemItem x : items) {
+		set.add(x.getOrder());
+		}
+		return set;
 	}
 
 	@Override
